7cf471403518929fe077d4a45e5bca29c6d3e2d1
jenstem <124745888+jenstem@users.noreply.github.com>
2023-08-19 19:06:11 -0400

    Revise id numbers in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,21 @@
-INSERT INTO department (id, name)
-VALUES (100, 'Accounting'),
-       (200, 'Legal'),
-       (300, 'Admin'),
-       (400, 'HR');
+INSERT INTO department (name)
+VALUES ('Accounting'),
+       ('Legal'),
+       ('Admin'),
+       ('HR');
 
 -- why isn't the department's id the same as department_id?
-INSERT INTO role (id, title, salary, department_id)
-VALUES (10, 'Accountant', 65000, 100),
-       (20, 'Attorney', 120000, 200),
-       (30, 'Receptionist', 65000, 300),
-       (40, 'Manager', 75000, 400);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 65000, 1),
+       ('Attorney', 120000, 2),
+       ('Receptionist', 65000, 3),
+       ('Manager', 75000, 4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1001, 'Amanda', 'Nunez', 10, 2010),
-       (2001, 'John', 'Martin', 20, 3010),
-       (3001, 'Gabrielle', 'Florence', 30, 3010),
-       (4001, 'Mara', 'Baldwin', 40, 2010),
-       (5001, 'Jonas', 'Wood', 40, 2010),
-       (6001, 'Raphael', 'Trujillo', 20, 2010),
-       (7001, 'Amira', 'Whitehead', 10, 2010);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Amanda', 'Nunez', 4, NULL),
+       ('John', 'Martin', 2, 1),
+       ('Gabrielle', 'Florence', 3, 1),
+       ('Mara', 'Baldwin', 4, NULL),
+       ('Jonas', 'Wood', 1, 4),
+       ('Raphael', 'Trujillo', 2, 4),
+       ('Amira', 'Whitehead', 3, 4);
